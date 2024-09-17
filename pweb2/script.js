2e1e49b5143c89d22011ed3eaf24c7aa865881c1
José Lucena <139528889+JoseLucenaa@users.noreply.github.com>
2024-09-17 13:53:52 -0300

    commit pweb2

---
+++ pweb2/script.js
@@ -0,0 +1,47 @@
+
+function addLinha() {
+    const tabela = document.getElementById('infoTable');
+    
+    const addtr = document.createElement('tr');
+    
+    const addnome = document.createElement('td');
+    addnome.textContent = 'João';
+    addtr.appendChild(addnome);
+    
+    const addidade = document.createElement('td');
+    addidade.textContent = '25';
+    addtr.appendChild(addidade);
+    
+    tabela.appendChild(addtr);
+}
+
+function modificarDiv() {
+
+    const contentDiv = document.getElementById('content');
+    
+    const header = contentDiv.querySelector('h2');
+    if (header) {
+        header.textContent = 'Conteúdo Atualizado';
+    }
+        
+    const paragrafo = document.createElement('p');
+    paragrafo.textContent = 'Este é um novo parágrafo adicionado.';
+    contentDiv.appendChild(paragrafo);
+}
+
+function adicionarBloco() {
+        const newDiv = document.createElement('div');
+        newDiv.innerHTML = `
+            <h3>Novo Bloco</h3>
+            <p>Este é um novo bloco adicionado.</p>
+        `;
+        document.getElementById('content').appendChild(newDiv);
+}
+
+
+function removerBloco(){
+    const paragraph = document.querySelector('#content p');
+        if (paragraph) {
+            paragraph.remove();
+        }
+}
